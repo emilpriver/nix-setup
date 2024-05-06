caec5a28fac9ba24a9e80797cd6d1055db28d29a
emilpriver <emil@priver.dev>
2024-05-06 21:22:28 +0200

    Init
    Signed-off-by: emilpriver <emil@priver.dev>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# 1. In this dir create a lockfile:
+#   nix flake lock --update-input nixpkgs
+#
+# 2. Install the profile from home dir
+#   nix profile install ~/.config/nixpkgs\#default
+#
+# 3. Update packages
+#   nix profile upgrade nixpkgs --impure
+
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = "${system}";
+        config.allowUnfree = true;
+      };
+    in {
+      packages.${system}.default = pkgs.buildEnv {
+        name = "tools";
+        paths = with pkgs; [ nix-direnv nixfmt-classic cachix xh fd slack tidal-hifi spotify discord ];
+      };
+    };
+}
