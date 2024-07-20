4d59c34d184052d7da48262197f67852f5023c98
emilpriver <emil@priver.dev>
2024-07-20 12:30:31 +0200

    Update flake to use nix overlays and ocaml 5.2

--- flake.nix
+++ flake.nix
@@ -6,55 +6,80 @@
 #
 # 3. Update packages
 #   nix profile upgrade nixpkgs --impure
-
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable"; };
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        system = "${system}";
-        config.allowUnfree = true;
-      };
-    in
-    {
-      formatter.${system} = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-      packages.${system}.default = pkgs.buildEnv {
-        name = "tools";
-        paths = with pkgs; [
-          nix-direnv
-          nixfmt-classic
-          cachix
-          xh
-          fd
-          tree
-          turso-cli
-          bun
-          ncdu
-          google-cloud-sql-proxy
-          fzf
-
-          # Programs
-          slack discord signal-desktop charm-freeze hoppscotch tor-browser gitkraken git-cliff terraform postman 
-
-          #ide
-          zed-editor neovim bruno
-
-          # ocaml
-          dune_3 ocaml ocamlPackages.findlib dune-release ocamlPackages.ocaml-lsp
-        
-          # golang
-          go gofumpt
-
-          # rust
-          rustup
-
-          # Javascript
-          nodePackages_latest.pnpm nodejs_22
-
-          # docker
-          docker docker-compose
-        ];
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    ocamlOverlay.url = "github:nix-ocaml/nix-overlays";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    ocamlOverlay,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      system = "${system}";
+      config.allowUnfree = true;
     };
+    ocamlPkgs = ocamlOverlay.legacyPackages.${system};
+  in {
+    formatter.${system} = pkgs.alejandra;
+    packages.${system}.default = pkgs.buildEnv {
+      name = "tools";
+      paths = with pkgs; [
+        nix-direnv
+        nixfmt-classic
+        cachix
+        xh
+        fd
+        tree
+        turso-cli
+        bun
+        ncdu
+        google-cloud-sql-proxy
+        fzf
+
+        # Programs
+        slack
+        discord
+        signal-desktop
+        charm-freeze
+        hoppscotch
+        tor-browser
+        gitkraken
+        git-cliff
+        terraform
+        postman
+
+        #ide
+        zed-editor
+        neovim
+        bruno
+
+        # ocaml
+        ocamlPkgs.dune_3
+        ocamlPkgs.opam
+        ocamlPkgs.ocaml
+        ocamlPkgs.ocamlPackages.findlib
+        ocamlPkgs.dune-release
+        ocamlPkgs.ocamlPackages.ocaml-lsp
+        ocamlPkgs.ocamlPackages.ocamlformat
+
+        # golang
+        go
+        gofumpt
+
+        # rust
+        rustup
+
+        # Javascript
+        nodePackages_latest.pnpm
+        nodejs_22
+
+        # docker
+        docker
+        docker-compose
+      ];
+    };
+  };
 }
